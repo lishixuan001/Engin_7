169036592c2259a2f297838b91e988bf643405cb
Wayne <lishixuan001@berkeley.edu>
2017-09-11 01:31:29 -0700

    Add files via upload

---
+++ Lab_Assignments/Lab 1/my_test_function.m
@@ -0,0 +1,8 @@
+function [result] = my_test_function(x)
+
+% E7 Spring 2017, University of California at Berkeley.
+% Function to submit as-is for question 2 of Lab Assignment 01.
+
+result = x + 2;
+
+end
